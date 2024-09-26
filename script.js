68401658dc1a36a3f407d5cd942f400f502f9421
Nandkishore Parmar <nandkishoreparmar97@gmail.com>
2024-09-26 19:49:37 +0530

    Navbar Completed

---
+++ script.js
@@ -0,0 +1,9 @@
+const sideMenu = document.getElementById('sideMenu'); 
+
+function openMenu(){
+    sideMenu.style.transform = 'translateX(-16rem)';
+}
+
+function closeMenu(){
+    sideMenu.style.transform = 'translateX(16rem)';
+}
